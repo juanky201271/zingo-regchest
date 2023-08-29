b3529bedcace59096ec06e41f9d78b07e84e2ffa
Oscar Pepper <oscarpepper@zingolabs.org>
2023-08-29 15:50:19 +0100

    updated lock and added loop

--- Dockerfile
+++ Dockerfile
@@ -81,7 +81,7 @@ RUN apt autoremove -y \
 # Copy rust into docker image and build (to be replaced with git clone)
 RUN mkdir /usr/src/zingo-regchest
 WORKDIR /usr/src/zingo-regchest
-# COPY ./Cargo.lock ./Cargo.lock
+COPY ./Cargo.lock ./Cargo.lock
 COPY ./Cargo.toml ./Cargo.toml
 COPY ./regtest ./regtest
 COPY ./src ./src
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 // use futures::prelude::*;
+use std::{thread, time};
 use zingo_testutils::{self, scenarios};
 
 #[tokio::main]
 async fn main() {
     let (_regtest_manager, _child_process_handler) =
         scenarios::funded_orchard_mobileclient(1_000_000).await;
+    loop {
+        thread::sleep(time::Duration::from_millis(100))
+    }
 }
